4d7df809fca0f5ddf9f7f420341fb4681c9544d2
Marcos Zavala <marcos.zavala@example.com>
2025-01-29 22:59:42 -0600

    Add production server configuration

---
+++ server.js
@@ -0,0 +1,1 @@
+const express = require("express");const path = require("path");const app = express();const PORT = process.env.PORT || 5173;app.use(express.static(path.join(__dirname, "dist")));app.get("*", (req, res) => {res.sendFile(path.join(__dirname, "dist", "index.html"));});app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`);});
